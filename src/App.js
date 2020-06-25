92276fc53ee816998f625fb707e8143e4ff760cb
stephenprosso <stephenprosso@gmail.com>
2020-06-25 14:59:53 -0700

    adding Css to format app, nav and menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-     Stephen Rosskito
-    </div>
-  );
+import React, { Component } from 'react';
+import './css/style.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        Stephen Rosskito
+        <nav>
+          <h2 class="logo">Oscars 2019</h2>
+          <ul class="nav-menu">
+            <li>Home</li>
+            <li>Actors</li>
+            <li>Actress</li>
+            <li>Films</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//      Stephen Rosskito
+//      <nav>
+//        <h2>Oscars 2019</h2>
+//        <ul>
+//          <li>Home</li>
+//          <li>Actors</li>
+//          <li>Actress</li>
+//          <li>Films</li>
+//        </ul>
+//      </nav>
+//     </div>
+//   );
+// }
 
 export default App;
