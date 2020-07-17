d39b4b1678e4a178c925f97f7e48b15d950b0735
stephenprosso <stephenprosso@gmail.com>
2020-07-17 12:03:59 -0700

    change artists back to Artists

--- src/components/ArtistBio.js
+++ src/components/ArtistBio.js
@@ -6,7 +6,7 @@ const ArtistBio = (props) => {
 
     return (
         <div className="main-content">
-            <div><Link className="back" to="/artists">Back</Link></div>
+            <div><Link className="back" to="/Artists">Back</Link></div>
             <h2>{props.title}</h2>
             <div className="container">
                 {/* the prop you access below needs to match the name of the prop passed in the function */}
